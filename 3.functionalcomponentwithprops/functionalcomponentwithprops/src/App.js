4519920938c3ff103df977a4881f92049f32be08
georgemathewk <georgemathewk@yahoo.com>
2023-10-13 11:57:52 +0530

    Funtional Components with props

---
+++ 3.functionalcomponentwithprops/functionalcomponentwithprops/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Hello World" subtitle="This is a subtitle" />
+    </div>
+  );
+}
+
+export default App;
---
+++ 3.functionalcomponentwithprops/functionalcomponentwithprops/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <header>
+            <h1>{props.title}</h1>
+            <p>{props.subtitle}</p>
+        </header>
+    );
+}
+
+export default Header;
