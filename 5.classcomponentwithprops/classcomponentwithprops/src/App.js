8cd15a82790f406d536fc9cd684904eae23f4b44
georgemathewk <georgemathewk@yahoo.com>
2023-10-13 15:31:38 +0530

    Props demo in class component

---
+++ 5.classcomponentwithprops/classcomponentwithprops/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Simple App" subtitle="Props demo in class components" />
+    </div>
+  );
+}
+
+export default App;
