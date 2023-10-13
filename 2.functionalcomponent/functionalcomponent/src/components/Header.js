0bd6156f1d09db68e70bb761065fec6d766d090b
georgemathewk <georgemathewk@yahoo.com>
2023-10-13 11:44:34 +0530

    Function Component Example

---
+++ 2.functionalcomponent/functionalcomponent/src/components/Header.js
@@ -0,0 +1,15 @@
+/**
+ * Header component that displays a header with a title.
+ * @returns {JSX.Element} JSX representation of the header component.
+ */
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <h1>This is the header component</h1>
+        </div>
+    );
+}
+
+export default Header;
